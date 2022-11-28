9861bb5a5cafdb2978fa75199ca421d15c162514
Tom Liang <tomliangg@gmail.com>
2022-11-27 20:51:45 -0800

    go http server to serve a html file

---
+++ cmd/web/handlers.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	http.ServeFile(w, r, "./ui/index.html")
+}
